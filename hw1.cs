32ce8ecad8dc5955a14bffe4d8fc734af179737f
profounddark <39137591+profounddark@users.noreply.github.com>
2023-04-04 19:06:54 -0700

    added some comments

--- hw1.cs
+++ hw1.cs
@@ -53,6 +53,15 @@
         }
     }
 
+    /// <summary>
+    /// FibRecursiveAccum calculates the nth Fibonacci number using a recursive
+    /// algorithm with acumulator variables.
+    /// </summary>
+    /// <param name="number">the number (in sequence) to return</param>
+    /// <param name="addCount">a counter for tracking additions</param>
+    /// <param name="a">the nth - 2 accumulator</param>
+    /// <param name="b">the nth - 1 accumulator</param>
+    /// <returns>the value of the nth fibonacci number</returns>
     public static int FibRecursiveAccum(int number, ref int addCount, int a = 0, int b = 1)
     {
         if (number <= 0)
@@ -67,6 +76,12 @@
         }
     }
 
+    /// <summary>
+    /// Main is the main entry point for the Fibonacci program. Runs tests on
+    /// three different Fibonacci algorithms and counts the number of additions
+    /// performed by each.
+    /// </summary>
+    /// <param name="args">command line arguments; not used here</param>
     public static void Main(string[] args)
     {
         int[] testValues = { 3, 10, 20 };
